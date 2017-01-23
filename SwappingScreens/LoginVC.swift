2c9a7aa777764e2623d1c7e602396afd92040c15
Syed Jilany <syedjilany@gmail.com>
2017-01-23 15:24:49 -0500

    done

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Syed Jilany on 1/23/17.
+//  Copyright © 2017 SyedJilany. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
---
+++ SwappingScreens/MusicListVC.swift
@@ -0,0 +1,50 @@
+//
+//  MusicListVC.swift
+//  SwappingScreens
+//
+//  Created by Syed Jilany on 1/23/17.
+//  Copyright © 2017 SyedJilany. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backBtnPRessed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Quit Playing Games With My Heart"
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            if let song = sender as? String {
+                destination.selectedSong = song
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
